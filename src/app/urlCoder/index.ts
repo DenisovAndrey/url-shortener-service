3348d0a73ac851a2a64b0b3a13a80455da467e99
Andrey Denisov <andrey.denisov@westwing.de>
2023-06-30 21:16:50 +0200

    move url service

--- src/app/urlCoder/index.ts
+++ src/app/urlCoder/index.ts
@@ -1,10 +1,3 @@
-import { Router } from 'express'
-import { UrlCoderPaths } from './routes/paths'
-import { UrlController } from './controllers/UrlController'
-
-const router = Router()
-
-router.get(UrlCoderPaths.ENCODE, UrlController.encodeUrl)
-router.get(UrlCoderPaths.DECODE, UrlController.decodeUrl)
+import router from './services/UrlService'
 
 export default router
---
+++ src/app/urlCoder/services/UrlService.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { UrlCoderPaths } from '../routes/paths'
+import { UrlController } from '../controllers/UrlController'
+
+const router = Router()
+
+router.get(UrlCoderPaths.ENCODE, UrlController.encodeUrl)
+router.get(UrlCoderPaths.DECODE, UrlController.decodeUrl)
+
+export default router
